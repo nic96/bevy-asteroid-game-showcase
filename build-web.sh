5635ff171f1d218ef6e6cb50e1c05631dd8a6963
nic96 <nic96@users.noreply.github.com>
2020-10-17 16:44:18 -0600

    Rename

--- build-web.sh
+++ build-web.sh
@@ -1,4 +1,4 @@
 cargo build --target wasm32-unknown-unknown --target-dir target
-wasm-bindgen --out-dir public/target --target web target/wasm32-unknown-unknown/debug/bevy_playground.wasm
+wasm-bindgen --out-dir public/target --target web target/wasm32-unknown-unknown/debug/bevy-asteroid-game-showcase.wasm
 cp -r assets public/
 python -m http.server -d public
